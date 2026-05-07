599d3e1bc18a2d7310a755af3c977721ff43473a
luka onikadze <lukaonik@gmail.com>
2026-05-07 18:40:16 +0400

    13

---
+++ 13.py
@@ -0,0 +1,5 @@
+def sort_employes(employes: list[dict]):
+    return sorted(employes, key=lambda e: e['salary'])
+
+employees = [{"name": "A", "salary": 50}, {"name": "B", "salary": 70}, {"name": "C", "salary": 60}]
+print(sort_employes(employees))
